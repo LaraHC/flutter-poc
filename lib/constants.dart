a2f22e6aab04e9d676b5108b867551d407cf66f3
LaraHC <59648381+LaraHC@users.noreply.github.com>
2021-08-25 12:50:26 +0200

    change colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF756EA0);
 const secondaryColor = Color(0xFFFFFFFF);
-const bgColor = Color(0xFFFFFFFF);
+const bgColor = Color.fromRGBO(240, 241, 246, 1);
 
 const primaryTextColor = Color(0xFF292A31);
 const secondaryTextColor = Color(0xFFC6C6C6);
 
 const primaryButton = Color(0xFF21CBC4);
 const secondaryButton = Color(0xFFE34747);
-
-
 
 const defaultPadding = 16.0;
 const profilePadding = 4.0;
