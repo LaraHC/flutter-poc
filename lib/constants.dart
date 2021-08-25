3713040cfe35bf55c0c6f730a11d0a2893720d35
LaraHC <59648381+LaraHC@users.noreply.github.com>
2021-08-25 16:25:07 +0200

    Add new colors to constants file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF756EA0);
-const secondaryColor = Color(0xFFFFFFFF);
+const secondaryColor = Color.fromRGBO(255, 255, 255, 1);
 const bgColor = Color.fromRGBO(240, 241, 246, 1);
 
-const primaryTextColor = Color(0xFF292A31);
+const primaryTextColor = Color.fromRGBO(104, 97, 97, 1);
 const secondaryTextColor = Color(0xFFC6C6C6);
 
 const primaryButton = Color(0xFF21CBC4);
 const secondaryButton = Color(0xFFE34747);
+
+const darkGrey = Color.fromRGBO(114, 114, 114, 1);
+const transparentWhite = Color.fromRGBO(255, 255, 255, .4);
 
 const defaultPadding = 16.0;
 const profilePadding = 4.0;
