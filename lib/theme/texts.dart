1cde22ddcd3095c2816d6ca616acd243bdf79d5e
LaraHC <59648381+LaraHC@users.noreply.github.com>
2021-08-26 16:19:19 +0200

    modify color and text name references

--- lib/theme/texts.dart
+++ lib/theme/texts.dart
@@ -5,28 +5,28 @@ import 'package:flutter/material.dart';
 /// Primary Font Styles (Open Sans)
 ///
 const primaryH1Bold = const TextStyle(
-    color: DarwinPrimaryDark,
+    color: HumanuiPrimaryDark,
     fontWeight: FontWeight.w700,
     fontFamily: 'Open Sans',
     fontStyle: FontStyle.normal,
     fontSize: 37.0);
 
 const primaryH1Regular = const TextStyle(
-    color: DarwinPrimaryDark,
+    color: HumanuiPrimaryDark,
     fontWeight: FontWeight.w300,
     fontFamily: 'Open Sans',
     fontStyle: FontStyle.normal,
     fontSize: 37.0);
 
 const primaryPRegular = const TextStyle(
-    color: DarwinPrimaryDark,
+    color: HumanuiPrimaryDark,
     fontWeight: FontWeight.w300,
     fontFamily: 'Open Sans',
     fontStyle: FontStyle.normal,
     fontSize: 16.0);
 
 const primaryPBold = const TextStyle(
-    color: DarwinPrimaryDark,
+    color: HumanuiPrimaryDark,
     fontWeight: FontWeight.w700,
     fontFamily: 'Open Sans',
     fontStyle: FontStyle.normal,
@@ -36,28 +36,28 @@ const primaryPBold = const TextStyle(
 /// Secondary Font Styles (Roboto)
 ///
 const secondaryH1Bold = const TextStyle(
-    color: DarwinPrimaryDark,
+    color: HumanuiPrimaryDark,
     fontWeight: FontWeight.w700,
     fontFamily: 'Roboto',
     fontStyle: FontStyle.normal,
     fontSize: 37.0);
 
 const secondaryH1Regular = const TextStyle(
-    color: DarwinPrimaryDark,
+    color: HumanuiPrimaryDark,
     fontWeight: FontWeight.w300,
     fontFamily: 'Roboto',
     fontStyle: FontStyle.normal,
     fontSize: 37.0);
 
 const secondaryPRegular = const TextStyle(
-    color: DarwinPrimaryDark,
+    color: HumanuiPrimaryDark,
     fontWeight: FontWeight.w300,
     fontFamily: 'Roboto',
     fontStyle: FontStyle.normal,
     fontSize: 16.0);
 
 const secondaryPBold = const TextStyle(
-    color: DarwinPrimaryDark,
+    color: HumanuiPrimaryDark,
     fontWeight: FontWeight.w700,
     fontFamily: 'Roboto',
     fontStyle: FontStyle.normal,
